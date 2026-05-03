520864247eb7367c5e52a7b389c308b0097360f0
ahodges2-debug <ahodges2@terpmail.umd.edu>
2026-05-03 15:09:42 -0400

    Update Practice_Assignment.py

--- Practice_Collab/Practice_Assignment.py
+++ Practice_Collab/Practice_Assignment.py
@@ -351,7 +351,7 @@ def get_available_events(gpa, **resources):
         reverse=True
     )
 
-    #conditional statement
+    #conditional expression
     top_choice = sorted_options[0] if sorted_options else "No events available"
 
     #f-string
